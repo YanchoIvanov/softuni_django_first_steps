0bad24cda79308ff15e6a3113a4deb91a45662e9
Yancho Ivanov <123395286+YanchoIvanov@users.noreply.github.com>
2024-01-06 09:59:46 +0200

    Add files via upload

---
+++ 17 AdvancedDjangoModelTechniquesExercise/main_app/mixin.py
@@ -0,0 +1,9 @@
+class RechargeEnergyMixin:
+
+    def recharge_energy(self, amount):
+        self.energy += amount
+
+        if self.energy > 100:
+            self.energy = 100
+
+        self.save()
